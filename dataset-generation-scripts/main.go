339019db1528899da989327874dccc0ab90e893a
Thang Le Quoc <thanglequoc.it@gmail.com>
2025-07-12 11:57:36 +0700

    feat: Restructure table
    - Remove districts table
    - Set the relationship between provinces and wards (2 levels administrative units)

--- dataset-generation-scripts/main.go
+++ dataset-generation-scripts/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	vn_common "github.com/thanglequoc-vn-provinces/v2/common"
-	dataset_writer "github.com/thanglequoc-vn-provinces/v2/dataset_writer"
-	dumper "github.com/thanglequoc-vn-provinces/v2/dumper"
+	// dataset_writer "github.com/thanglequoc-vn-provinces/v2/dataset_writer"
+	// dumper "github.com/thanglequoc-vn-provinces/v2/dumper"
 )
 
 func main() {
 	// pre-run
 	// Refresh temporary dataset, import existing dataset
 	vn_common.BootstrapTemporaryDatasetStructure()
-	dumper.BeginDumpingDataWithDvhcvnDirectSource()
-	dataset_writer.ReadAndGenerateSQLDatasets()
+	// dumper.BeginDumpingDataWithDvhcvnDirectSource()
+	// dataset_writer.ReadAndGenerateSQLDatasets()
 }
