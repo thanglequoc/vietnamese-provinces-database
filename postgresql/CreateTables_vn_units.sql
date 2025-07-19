8cd509f5ff48d326574cc52090f99bda42eba494
Thang Le Quoc <thanglequoc.it@gmail.com>
2025-07-19 11:19:38 +0700

    Update CreateTables schema scripts
    - Refactor SQL schema creation scripts, remove districts table
    - Remove province-region relationship

--- postgresql/CreateTables_vn_units.sql
+++ postgresql/CreateTables_vn_units.sql
@@ -1,5 +1,4 @@
 -- DROP TABLE IF EXISTS wards;
--- DROP TABLE IF EXISTS districts;
 -- DROP TABLE IF EXISTS provinces;
 -- DROP TABLE IF EXISTS administrative_units;
 -- DROP TABLE IF EXISTS administrative_regions;
@@ -37,42 +36,13 @@ CREATE TABLE provinces (
 	full_name_en varchar(255) NULL,
 	code_name varchar(255) NULL,
 	administrative_unit_id integer NULL,
-	administrative_region_id integer NULL,
 	CONSTRAINT provinces_pkey PRIMARY KEY (code)
 );
 
 
 -- provinces foreign keys
-
-ALTER TABLE provinces ADD CONSTRAINT provinces_administrative_region_id_fkey FOREIGN KEY (administrative_region_id) REFERENCES administrative_regions(id);
 ALTER TABLE provinces ADD CONSTRAINT provinces_administrative_unit_id_fkey FOREIGN KEY (administrative_unit_id) REFERENCES administrative_units(id);
-
-CREATE INDEX idx_provinces_region ON provinces(administrative_region_id);
 CREATE INDEX idx_provinces_unit ON provinces(administrative_unit_id);
-
-
--- CREATE districts TABLE
-CREATE TABLE districts (
-	code varchar(20) NOT NULL,
-	"name" varchar(255) NOT NULL,
-	name_en varchar(255) NULL,
-	full_name varchar(255) NULL,
-	full_name_en varchar(255) NULL,
-	code_name varchar(255) NULL,
-	province_code varchar(20) NULL,
-	administrative_unit_id integer NULL,
-	CONSTRAINT districts_pkey PRIMARY KEY (code)
-);
-
-
--- districts foreign keys
-
-ALTER TABLE districts ADD CONSTRAINT districts_administrative_unit_id_fkey FOREIGN KEY (administrative_unit_id) REFERENCES administrative_units(id);
-ALTER TABLE districts ADD CONSTRAINT districts_province_code_fkey FOREIGN KEY (province_code) REFERENCES provinces(code);
-
-CREATE INDEX idx_districts_province ON districts(province_code);
-CREATE INDEX idx_districts_unit ON districts(administrative_unit_id);
-
 
 
 -- CREATE wards TABLE
@@ -83,7 +53,7 @@ CREATE TABLE wards (
 	full_name varchar(255) NULL,
 	full_name_en varchar(255) NULL,
 	code_name varchar(255) NULL,
-	district_code varchar(20) NULL,
+	province_code varchar(20) NULL,
 	administrative_unit_id integer NULL,
 	CONSTRAINT wards_pkey PRIMARY KEY (code)
 );
@@ -92,7 +62,7 @@ CREATE TABLE wards (
 -- wards foreign keys
 
 ALTER TABLE wards ADD CONSTRAINT wards_administrative_unit_id_fkey FOREIGN KEY (administrative_unit_id) REFERENCES administrative_units(id);
-ALTER TABLE wards ADD CONSTRAINT wards_district_code_fkey FOREIGN KEY (district_code) REFERENCES districts(code);
+ALTER TABLE wards ADD CONSTRAINT wards_province_code_fkey FOREIGN KEY (province_code) REFERENCES provinces(code);
 
-CREATE INDEX idx_wards_district ON wards(district_code);
+CREATE INDEX idx_wards_province ON wards(province_code);
 CREATE INDEX idx_wards_unit ON wards(administrative_unit_id);
