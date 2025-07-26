ba1ff795486a0c6f009a9302325382ab2de162b9
Thang Le Quoc <thanglequoc.it@gmail.com>
2025-07-26 19:59:21 +0700

    Add service API to get data from sapnhap site

---
+++ dataset-generation-scripts/sapnhap_bando/fetcher/fetcher.go
@@ -0,0 +1,27 @@
+package fetcher
+
+import (
+	"encoding/json"
+	"net/http"
+	"github.com/thanglequoc-vn-provinces/v2/sapnhap_bando/dto"
+)
+
+const GET_ALL_PROVINCES_URL = "https://sapnhap.bando.com.vn/pcotinh"
+
+/*
+Get all the provinces data from the sapnhap site
+POST: https://sapnhap.bando.com.vn/pcotinh
+*/
+func GetAllProvincesDataFromSapNhapSite() []dto.SapNhapProvinceData {
+	res, err := http.Post(GET_ALL_PROVINCES_URL, "application/json", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+	var provincesData []dto.SapNhapProvinceData
+	if err := json.NewDecoder(res.Body).Decode(&provincesData); err != nil {
+		panic(err)
+	}
+	return provincesData
+}
