d7fdfed8b5afac3030a4e970e59fb0041c7b1eb9
Thang Le Quoc <thanglequoc.it@gmail.com>
2026-07-10 23:16:34 +0700

    feat: 169: Clean up old code reference
    - Clean up the old GIS data bootstrap code that use the API and response model from the past sapnhap bando site

--- dataset-generation-scripts/internal/sapnhap_bando/sapnhap_bando.go
+++ dataset-generation-scripts/internal/sapnhap_bando/sapnhap_bando.go
@@ -2,10 +2,7 @@ package sapnhapbando
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"os"
-	"strconv"
 
 	db "github.com/thanglequoc-vn-provinces/v2/internal/database"
 
@@ -13,59 +10,6 @@ import (
 	sapNhapService "github.com/thanglequoc-vn-provinces/v2/internal/sapnhap_bando/service"
 	vnRepo "github.com/thanglequoc-vn-provinces/v2/internal/vn_provinces_tmp/repository"
 )
-
-const (
-	PROVINCE_GIS_EXISTING_DUMP_PATH = "./resources/gis/exported/sapnhap_provinces_gis_202509011857_lfs.sql"
-	WARD_GIS_EXISTING_DUMP_PATH     = "./resources/gis/exported/sapnhap_wards_gis_202509011858_lfs.sql"
-)
-
-func DumpDataFromSapNhapBando() {
-	// Initialize the SapNhapRepository
-	postgresDB := db.GetPostgresDBConnection()
-	sapNhapRepo := sapNhapR.NewSapNhapRepository(postgresDB)
-	sapNhapGISRepo := sapNhapR.NewSapNhapGISRepository(postgresDB)
-	sapNhapGeoJSONObjectRepository := sapNhapR.NewSapNhapGeoJSONObjectRepository(postgresDB)
-	vnRepo := vnRepo.NewVnProvincesTmpRepository(postgresDB)
-
-	sapNhapService := sapNhapService.NewSapNhapService(sapNhapRepo, sapNhapGISRepo, vnRepo, sapNhapGeoJSONObjectRepository, postgresDB)
-
-	if err := sapNhapService.BootstrapSapNhapSiteProvinces(); err != nil {
-		log.Fatalf("Failed to dump SapNhapSiteProvinces: %v", err)
-		panic(err)
-	}
-
-	if err := sapNhapService.BootstrapSapNhapSiteWards(); err != nil {
-		log.Fatalf("Failed to dump SapNhapSiteWards: %v", err)
-		panic(err)
-	}
-
-	shouldGetGISFromDumpSQLPatch, err := strconv.ParseBool(os.Getenv("DUMP_GIS_RESOURCE_FROM_EXISTING_DB_PATCH"))
-	if err != nil {
-		fmt.Println("Error parsing DUMP_GIS_RESOURCE_FROM_EXISTING_DB_PATCH, default to false")
-		shouldGetGISFromDumpSQLPatch = false
-	}
-
-	if shouldGetGISFromDumpSQLPatch {
-		log.Println("ℹ️ DUMP_GIS_RESOURCE_FROM_EXISTING_DB_PATCH is set to true, will load GIS data from existing SQL dump file")
-		// Just go ahead and execute the existing SQL dump file
-		if err := db.ExecuteSQLScript(PROVINCE_GIS_EXISTING_DUMP_PATH); err != nil {
-			log.Fatalf("Failed to execute province GIS dump script: %v", err)
-			panic(err)
-		}
-		if err := db.ExecuteSQLScript(WARD_GIS_EXISTING_DUMP_PATH); err != nil {
-			log.Fatalf("Failed to execute ward GIS dump script: %v", err)
-			panic(err)
-		}
-	} else {
-		log.Println("ℹ️ DUMP_GIS_RESOURCE_FROM_EXISTING_DB_PATCH is set to false, will fetch GIS data from GIS server")
-		if err := sapNhapService.BootstrapGISDataFromGISServer(); err != nil {
-			log.Fatalf("Failed to bootstrap GIS Data: %v", err)
-			panic(err)
-		}
-	}
-
-	log.Println("✅ Data dump from SapNhap Bando completed successfully")
-}
 
 func FetchGISDataFromSapNhapBando() {
 	// Initialize repository
